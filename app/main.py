44639a8b9f4645ecd77a71a216f9726ed1bb8578
vasile.razdalovschi <vasile.razdalovschi@gmail.com>
2023-01-22 18:10:19 +0200

    handle exit code

--- app/main.py
+++ app/main.py
@@ -9,6 +9,7 @@ def main():
     completed_process = subprocess.run([command, *args], capture_output=True)
     sys.stdout.buffer.write(completed_process.stdout)
     sys.stderr.buffer.write(completed_process.stderr)
+    sys.exit(completed_process.returncode)
 
 
 if __name__ == "__main__":
